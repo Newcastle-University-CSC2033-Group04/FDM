ec49cf9300d1e76f6ab9160536020bc73d1d33d7
William Moses <w.moses1@newcastle.ac.uk>
2021-12-07 15:03:04 +0000

    Added contact page, style and button.

--- app.py
+++ app.py
@@ -26,6 +26,11 @@ def login():
     return render_template('login.html')
 
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
