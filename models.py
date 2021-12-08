33eb009c70cc9b0e919fbd95e65253656b2adbc5
AbdullaRashed <b9067959@newcastle.ac.uk>
2021-12-08 13:42:11 +0000

    created python file models.py

---
+++ models.py
@@ -0,0 +1,14 @@
+from app import db
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'Users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
