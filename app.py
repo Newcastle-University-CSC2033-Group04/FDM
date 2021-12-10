4c5f436c011897ad3b16260a81c74f59bbbd52e2
AbdullaRashed <b9067959@newcastle.ac.uk>
2021-12-10 12:35:34 +0000

    login_manager added so flask_login and application work together

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-
-
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -32,7 +33,16 @@ def contact():
 
 
 if __name__ == '__main__':
+    login_manager = LoginManager()
+    login_manager.login_view = 'users.login'
+    login_manager.init_app(app)
+
+    from models import User
+
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+
     app.run(debug=True)
-
-
-
