35fd9382cfa9f9ecc5f88cc252feda07af6888c4
AbdullaRashed <b9067959@newcastle.ac.uk>
2021-12-16 12:14:56 +0000

    Validation errors for register

--- user/forms.py
+++ user/forms.py
@@ -4,14 +4,30 @@ from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
 
 
+def character_check(form, field):
+    excluded_chars = "* ? ! ' ^ + % & / ( ) = } ] [ { $ # @ < >"
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(
+                f"Character {char} is not allowed.")
+
+
 class RegisterForm(FlaskForm):  # register form containing all the input fields
     username = StringField(validators=[DataRequired()])
-    email = StringField(validators=[DataRequired()])
-    firstname = StringField(validators=[DataRequired()])
-    lastname = StringField(validators=[DataRequired()])
-    password = PasswordField(validators=[DataRequired()])
-    confirm_password = PasswordField(validators=[DataRequired()])
+    email = StringField(validators=[DataRequired(), Email()])
+    firstname = StringField(validators=[DataRequired(), character_check])
+    lastname = StringField(validators=[DataRequired(), character_check])
+    password = PasswordField(validators=[DataRequired(),
+                                         Length(min=8, max=16, message='Password must be between 8 and 16 characters.')])
+    confirm_password = PasswordField(validators=[DataRequired(),
+                                                 EqualTo('password', message='Both password fields must be equal!')])
     submit = SubmitField()
+
+    def validate_password(self, password):
+        pa = re.compile(r'(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@<>?/.,"£$%^&=*():;+{}|_-])')
+        if not pa.match(self.password.data):
+            raise ValidationError("Password must contain at least: 1 digit, 1 special character,"
+                                  " 1 lowercase and 1 uppercase letter.")
 
 
 class LoginForm(FlaskForm):
