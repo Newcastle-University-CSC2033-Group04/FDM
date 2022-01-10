a75af86faf511d8362fae1384220cc0c112eeb77
Koufuku5K <wmoses5k@gmail.com>
2022-01-10 22:18:06 +0000

    Enlarged button hitbox.

--- static/javascript/game1.js
+++ static/javascript/game1.js
@@ -84,11 +84,11 @@ choices.forEach(choice => {
             incrementScore(SCORE_POINTS);
         }
 
-        selectedChoice.parentElement.classList.add(classToApply)
+        selectedChoice.parentElement.classList.add(classToApply);
 
         setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
         }, 1000)
     })
 })
@@ -99,34 +99,3 @@ incrementScore = num => {
 }
 
 startGame();
-
-var character =
-    document.getElementById("character");
-
-var block =
-    document.getElementById("block");
-
-function jump() {
-    // fixes animation restarting when spamming left click
-    if (character.classList.contains("animate")) {
-        return
-    }
-    // adds class animate if animate is not added yet
-    if(character.classList !== "animate") {
-        character.classList.add("animate");
-    }
-    // removes class animate so it allows users to jump more than once
-    setTimeout(function(){
-        character.classList.remove("animate");
-    }, 500);
-}
-
-var checkDead = setInterval(function(){
-    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if (blockLeft < 40 && blockLeft > 0 && characterTop >= 260) {
-        block.style.animation = "none";
-        block.style.display = "none";
-        alert("You Lose!");
-    }
-}, 10);
